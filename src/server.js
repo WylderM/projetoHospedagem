0af6b4e533919f68f2a1b2c5764e3e0742503822
WylderM <wyldermenezes@gmail.com>
2020-11-24 00:51:10 -0300

    em dev

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const path = require('path')
+const app = express()
 
-const app = express().use(bodyParser.json())
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs');
-const http = require('http').Server(app)
+
 //const cors = require('cors')
-
 enableCors = (req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Credentials', true)
@@ -15,8 +11,8 @@ enableCors = (req, res, next) => {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 	next()
 }
-// parse application/x-www-form-urlencoded
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(enableCors)
@@ -24,15 +20,15 @@ PORT = 4500
 HOST = '0.0.0.0'
 
 //Rotas
-//app.use('/', require('./routes'))
+app.use('/', require('./routes'))
 
 //views rotas
 app.get('/', function(req, res){
-	res.render('formularioHospedagem/index')
+	res.sendFile(__dirname + '/views/formularioHospedagem/teste.html')
 })
 
 //Server externo
-http.listen(PORT, function () {
+app.listen(PORT, function () {
 	console.log('API rodando na porta', PORT + '!')
 })
 
